9fbc51f21c4a5b27c5c6d4b9e62e1ad95f1dc44b
AmanP143 <118849404+AmanP143@users.noreply.github.com>
2022-11-30 00:54:47 +0530

    Update _02_changeOfAmount.py

--- _02_changeOfAmount.py
+++ _02_changeOfAmount.py
@@ -18,6 +18,8 @@ def make_amount(rupees_to_make,no_of_five,no_of_one):
 		print(-1)
 	else:
 		five_needed=int(rupees_to_make/5)
+		if(five_needed>=no_of_five):
+			 five_needed=five_needed-1
 		one_needed=rupees_to_make-(5*five_needed)
 		print("No. of Five needed :", five_needed)
 		print("No. of One needed  :", one_needed)
